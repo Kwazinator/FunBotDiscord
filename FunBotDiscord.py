351d4b4830db8c0b995496f9ea2303d3406d1529
Derek Schultz <schultzder@uwplatt.edu>
2025-01-09 11:05:55 -0600

    Added functionality that automatically looks at messages and moves them to a clips channel if they contain a clip.

--- FunBotDiscord.py
+++ FunBotDiscord.py
@@ -1,4 +1,6 @@
 import discord
+#import RiotConts as Conts
+#import requests
 from urllib.parse import quote_plus
 from hell_let_loose import HLL_View
 from discord.ext import commands, tasks
@@ -11,6 +13,9 @@ intents = discord.Intents.all()
 f = open("botToken.txt", "r")
 TOKEN = f.read()
 bot = commands.Bot(command_prefix="/", intents=intents)
+# List of valid video file extensions for Discord clips
+VALID_CLIP_EXTENSIONS = ['.mp4', '.webm', '.mov']
+CLIP_CHANNEL_ID = 1326956425762570240
 
 def setup_database():
     # Connect to SQLite database
@@ -37,6 +42,20 @@ class Google(discord.ui.View):
         # We add the quoted url to the button, and add the button to the view.
         self.add_item(discord.ui.Button(label='Click Here', url=url))
 
+# class RiotAPI(object):
+#     def __init__(self, api_key, region-Conts.REGIONS[]):
+#         self.api_key = api_key
+#         self.region = region
+#     def _request(self, api_url, params=()):
+#         args = {'api_key': self.api_key}
+#         for key, value in params.items():
+#             if key not in args:
+#                 args[key] = value
+#         try:        
+#             response = requests.get(Conts.URL['base'].format(proxy=self.region, region=self.region, url=api_url), params=args)
+#         except Exception as e:
+#             print(e)
+#         return response.json()
 @bot.event
 async def on_ready():
     print("Ready to go")
@@ -46,6 +65,36 @@ async def on_ready():
     except Exception as e:
         print(e)
     check_due_reminders.start()  # Start the reminders background task
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    # Ignore bot messages
+    if message.author.bot:
+        return
+
+    # Check if the message contains attachments
+    if message.attachments:
+        for attachment in message.attachments:
+            # Check if the attachment is a valid clip (based on file extension)
+            if any(attachment.filename.endswith(ext) for ext in VALID_CLIP_EXTENSIONS):
+                # Get the clip forwarding channel
+                clip_channel = bot.get_channel(CLIP_CHANNEL_ID)
+                if clip_channel:
+                    # Forward the message and attachment to the designated channel
+                    embed = discord.Embed(
+                        description=f"**{message.author.name}** shared a clip:",
+                        color=discord.Color.blue()
+                    )
+                    # Attach the clip URL in the embed
+                    embed.add_field(name="Clip", value=attachment.url, inline=False)
+                    await clip_channel.send(embed=embed)
+
+    # Don't forget to process commands if the bot has any
+    await bot.process_commands(message)
 
 @bot.hybrid_command(name="ping")
 async def hello(ctx: commands.Context):
---
+++ RiotConsts.py
@@ -0,0 +1,4 @@
+##URL = {'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}'
+  ##     'summoner_by_name': 'v{version}/summoner/by-name/{names}'}
+
+##API_VERSIONS = {'summoner': }
